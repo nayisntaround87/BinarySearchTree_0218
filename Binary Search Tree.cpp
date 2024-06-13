580f14b553ca2711c38b6f53b10a04ce1ec29e32
nayisntaround87 <naylasalwahayati@gmail.com>
2024-06-13 15:53:04 +0700

    void inorder

--- Binary Search Tree.cpp
+++ Binary Search Tree.cpp
@@ -73,6 +73,16 @@ public :
 
     void inorder(Node* ptr)
     {
-        
+        if (ROOT == NULL)
+        {
+            cout << "Tree is empty" << endl;
+            return;
+        }
+        if (ptr != NULL)
+        {
+            inorder(ptr->leftchild);
+            cout << ptr->info << " ";
+            inorder(ptr->rightchild);
+        }
     }
 };
