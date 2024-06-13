78bd49dfbe9262f802db184656c1984dd8db1094
nayisntaround87 <naylasalwahayati@gmail.com>
2024-06-13 15:22:23 +0700

    make the right child of the new node point to null

--- Binary Search Tree.cpp
+++ Binary Search Tree.cpp
@@ -33,5 +33,6 @@ public :
         Node* newNode = new Node(element, nullptr, nullptr); // alocate memory for the new node
         newNode->info = element; // assign value to the data field of the new node
         newNode->leftchild = nullptr; // make the left child of the new node point to NULL
+        newNode->rightchild = nullptr; // make the right child of the new node point to null
     } 
 };
