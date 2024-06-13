788e4f4553e49a465f51e689ac98f810b626ba36
nayisntaround87 <naylasalwahayati@gmail.com>
2024-06-13 15:24:22 +0700

    locate the node which will be the parent of the node to be insert

--- Binary Search Tree.cpp
+++ Binary Search Tree.cpp
@@ -34,5 +34,9 @@ public :
         newNode->info = element; // assign value to the data field of the new node
         newNode->leftchild = nullptr; // make the left child of the new node point to NULL
         newNode->rightchild = nullptr; // make the right child of the new node point to null
+
+        Node* parent = nullptr;
+        Node* currentNode = nullptr;
+        search(element, parent, currentNode); // locate the node which will be the parent of the node to be insert
     } 
 };
